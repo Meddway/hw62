bc3fceaa12939fe007dcd86826c9e64d49e9930b
Oleg Medvedev <meddway@gmail.com>
2023-11-29 19:20:05 +0600

    Добавлен компонент NotFound для отображения информации о том что указанная страница не найдена

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import Contacts from './pages/Contacts';
+import NotFound from './pages/NotFound';
 
 
 const App = () => {
@@ -11,10 +12,9 @@ const App = () => {
         <Route path="/" element={(<Home/>)}/>
         <Route path="/сontacts" element={(<Contacts/>)}/>
         <Route path="/aboutUs" element={(<AboutUs/>)}/>
+        <Route path="*" element={(<NotFound/>)}/>
       </Routes>
     </>
-
-
   );
 };
 
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+
+const NotFound = () => {
+  return (
+    <div className="container mt-3">
+      <h4>Error 404</h4>
+      <p>К сожалению, запрашиваемая страница не существует :(</p>
+    </div>
+
+
+  );
+};
+
+export default NotFound;
